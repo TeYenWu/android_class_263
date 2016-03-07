3830e4d029dbd297f12a9de1589179478cee384a
TeYanWu <killerw74@gmail.com>
2016-03-07 23:21:40 +0800

    add edit view and hidecheckbox , set onketlistener, add submit function

--- SimpleUI/app/src/main/java/com/example/simpleui/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/simpleui/simpleui/MainActivity.java
@@ -2,12 +2,69 @@ package com.example.simpleui.simpleui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
+    EditText editText;
+    CheckBox hideCheckBox;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView)findViewById(R.id.textView);
+        editText = (EditText)findViewById(R.id.editText);
+
+        editText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN) {
+                    submit(v);
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId == EditorInfo.IME_ACTION_DONE)
+                {
+                    submit(v);
+                    return true;
+                }
+
+                return false;
+            }
+        });
+
+        hideCheckBox = (CheckBox)findViewById(R.id.checkBox);
+
+    }
+
+    public  void submit(View view)
+    {
+        String text = editText.getText().toString();
+        if (hideCheckBox.isChecked())
+        {
+            Toast.makeText(this,text,Toast.LENGTH_LONG).show();
+            textView.setText("**********");
+            editText.setText("**********");
+            return;
+        }
+        editText.setText("");
+        textView.setText(text);
+
     }
 }
