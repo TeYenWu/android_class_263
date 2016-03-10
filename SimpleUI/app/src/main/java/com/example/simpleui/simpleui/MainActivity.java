19fba9981c0dd6d6d62fc0d86a9ca3f0ba9f8e30
TeYanWu <killerw74@gmail.com>
2016-03-10 21:51:49 +0800

    add spinner and list view

--- SimpleUI/app/src/main/java/com/example/simpleui/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/simpleui/simpleui/MainActivity.java
@@ -1,37 +1,59 @@
 package com.example.simpleui.simpleui;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import org.w3c.dom.Text;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
     EditText editText;
     CheckBox hideCheckBox;
+    SharedPreferences sp;
+    SharedPreferences.Editor editor;
+    ListView historyListView;
+    Spinner storeInfoSpinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        sp = getSharedPreferences("settings", Context.MODE_PRIVATE);
+        editor = sp.edit();
+
         textView = (TextView)findViewById(R.id.textView);
         editText = (EditText)findViewById(R.id.editText);
+
+        editText.setText(sp.getString("inputText", ""));
 
         editText.setOnKeyListener(new View.OnKeyListener() {
             @Override
             public boolean onKey(View v, int keyCode, KeyEvent event) {
+                String text = editText.getText().toString();
+                editor.putString("inputText", text);
+                editor.apply();
+
                 if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN) {
                     submit(v);
                     return true;
                 }
+
                 return false;
             }
         });
@@ -39,8 +61,7 @@ public class MainActivity extends AppCompatActivity {
         editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if(actionId == EditorInfo.IME_ACTION_DONE)
-                {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
                     submit(v);
                     return true;
                 }
@@ -51,11 +72,46 @@ public class MainActivity extends AppCompatActivity {
 
         hideCheckBox = (CheckBox)findViewById(R.id.checkBox);
 
+        hideCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                editor.putBoolean("hideCheckBox", hideCheckBox.isChecked());
+                editor.apply();
+            }
+        });
+
+        hideCheckBox.setChecked(sp.getBoolean("hideCheckBox", false));
+
+        historyListView = (ListView)findViewById(R.id.listView);
+        setHistory();
+
+
+        storeInfoSpinner = (Spinner)findViewById(R.id.spinner);
+        setStoreInfos();
+    }
+
+    private void setHistory()
+    {
+        String[] data = Utils.readFile(this, "history.txt").split("\n");
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data);
+        historyListView.setAdapter(adapter);
+
+    }
+
+    private void setStoreInfos()
+    {
+
+        String[] data = getResources().getStringArray(R.array.storeInfo);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, data);
+        storeInfoSpinner.setAdapter(adapter);
     }
 
     public  void submit(View view)
     {
         String text = editText.getText().toString();
+        Utils.writeFile(this, "history.txt", text + '\n');
+
+//        text = Utils.readFile(this, "history.txt");
         if (hideCheckBox.isChecked())
         {
             Toast.makeText(this,text,Toast.LENGTH_LONG).show();
